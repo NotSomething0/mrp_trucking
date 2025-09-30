30ecdd479707c8696b8be7d0a7866d557b9910fd
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2025-09-30 10:48:20 -0500

    docs(types): update type definitions

--- types.lua
+++ types.lua
@@ -2,46 +2,50 @@
 
 ---@alias CDeliveryRoute CRoutePointToPoint|CRouteMultiPointFueling
 
----Creates a new instance of CDepot
+---Create a new instance of CDepot
 ---@see CDepot.constructor
----@param blipCoordinate vector3
+---@param config CConfigStore
 ---@return CDepot
-function CDepot:new(blipCoordinate) end
+function CDepot:new(config) end
 
----comment
+---Create a new instance of CDeliveryManager
+---@see CDeliveryManager.constructor
 ---@param config CConfigStore
 ---@return CDeliveryManager
 function CDeliveryManager:new(config) end
 
----comments
+---Create a new instance of CDriverManager
+---@see CDriverManager.constructor
 ---@param config CConfigStore
 ---@param deliveryManager CDeliveryManager
 ---@return CDriverManager
 function CDriverManager:new(config, deliveryManager) end
 
----Creates a new instance of CDeliveryController
+---Create a new instance of CDeliveryController
+---@see CDeliveryController.constructor
 ---@param config CConfigStore
 ---@return CDeliveryController
 function CDeliveryController:new(config)
 end
 
----Creates a new instance of CConfigStore
+---Create a new instance of CConfigStore
 ---@see CConfigStore.constructor
 ---@return CConfigStore
 function CConfigStore:new()
 end
 
----Creates a new instance of CRouteFactory
+---Create a new instance of CRouteFactory
 ---@see CRouteFactory.constructor
 ---@return CRouteFactory
 function CRouteFactory:new() end
 
----comment
+---Create a new instance of CDriver
+---@see CDriver.constructor
 ---@param playerIndex number
 ---@return CDriver
 function CDriver:new(playerIndex) end
 
----Creates a new instance of CBaseRoute
+---Create a new instance of CBaseRoute
 ---@see CBaseRoute.constructor
 ---@param routeIndex number
 ---@param routeType RouteTypes
@@ -50,7 +54,7 @@ function CDriver:new(playerIndex) end
 ---@return CBaseRoute baseRoute
 function CBaseRoute:new(routeIndex, routeType, routeName, trailerCoordinates) end
 
----Creates a new instance of CRoutePointToPoint
+---Create a new instance of CRoutePointToPoint
 ---@see CRoutePointToPoint.constructor 
 ---@param baseRoute CBaseRoute
 ---@return CRoutePointToPoint route
