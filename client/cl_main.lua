ea2c46c6c4d8c82a55c6943c87ddd6ed435d47e1
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2026-06-17 18:32:36 -0500

    fix(cl_main): add missing text entry

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -67,16 +67,14 @@ AddEventHandler('onClientResourceStart', function(resourceName)
         return
     end
 
-    AddTextEntry('TJ_UNKNOWN_ERROR',
-        'An unknown error occured try repeating your previous action or contact development if this issue persists.')
+    AddTextEntry('TJ_UNKNOWN_ERROR', 'An unknown error occured try repeating your previous action or contact development if this issue persists.')
     AddTextEntry('TJ_ALREADY_CLOCKED_IN', 'You\'re already clocked in your vehicle should be nearby.')
     AddTextEntry('TJ_ALREADY_CLOCKED_OUT', 'You can not clock out as you were never clocked in.')
     AddTextEntry('TJ_SHIFT_ALREADY_STARTED', 'You already began your shift your equipment should be nearby.')
 
-    AddTextEntry('TJ_NO_TRK_SPAWN',
-        'No truck spawn avalible wait a couple minutes and retry. Alternatively contact staff if the issue persists.')
-    AddTextEntry('TJ_NO_TRL_SPAWN',
-        'No trailer spawn avalible wait a couples minutes and retry. Alternatively contact staff if the issue persists.')
+    AddTextEntry('TJ_NO_ROUTES_AVAILABLE', 'All delivery routes are currently occupied. Please wait a few minutes for a route to clear and try again.')
+    AddTextEntry('TJ_NO_TRK_SPAWN', 'No truck spawn available wait a couple minutes and retry. Alternatively contact staff if the issue persists.')
+    AddTextEntry('TJ_NO_TRL_SPAWN', 'No trailer spawn available wait a couple minutes and retry. Alternatively contact staff if the issue persists.')
 
     AddTextEntry('TJ_FAILED_TO_ASSIGN_ROUTE', 'Failed to assign route you have been clocked out')
 
@@ -86,20 +84,19 @@ AddEventHandler('onClientResourceStart', function(resourceName)
     AddTextEntry('TJ_DELIVER_TRAILER', 'Deliver the trailer to the marked ~HUD_COLOUR_YELLOW~~BLIP_ON_MISSION~~s~ location on your map.')
     AddTextEntry('TJ_TRAILER_TOO_FAR', 'The receiving client thinks the trailer is too far from the drop point. Wait for your trailer to be reconnected and move closer.')
 
-    AddTextEntry('TJ_RETURN_TO_DEPOT', 'Return to the depot ~HUD_COLOUR_ORANGEDARK~~BLIP_CAPTURE_THE_FLAG~~s~ to recieve your payment.')
+    AddTextEntry('TJ_RETURN_TO_DEPOT', 'Return to the depot ~HUD_COLOUR_ORANGEDARK~~BLIP_CAPTURE_THE_FLAG~~s~ to receive your payment.')
     AddTextEntry('TJ_RETURN_TRUCK', 'Return your delivery vehicle to the point marked ~HUD_COLOUR_YELLOW~~BLIP_ON_MISSION~~s~  on your map')
 
     AddTextEntry('TJ_HINT_DROP_TRAILER', 'Hold ~INPUT_VEH_ROOF~ to drop your trailer when the marker turns green.')
 
     AddTextEntry('TJ_RETURN_TO_MANAGER', 'Speak with the Post OP Manager ~HUD_COLOUR_ORANGEDARK~~BLIP_VIP~~s~ to complete your shift.')
-    AddTextEntry('TJ_PAYMENT_RECIEVE', 'Great Work! For finishing ~1~ deliveries you have been payed ~HUD_COLOUR_GREEN~~1~~s~.')
+    AddTextEntry('TJ_PAYMENT_RECEIVE', 'Great Work! For finishing ~1~ deliveries you have been paid ~HUD_COLOUR_GREEN~~1~~s~.')
     AddTextEntry('TJ_TIP_SPEAK_TO_MANAGER', 'Speak with the Post OP Depot Manager ~HUD_COLOUR_ORANGEDARK~~BLIP_VIP~~s~ to start your shift.')
 
-    AddTextEntry('TJ_TRAILER_DISCONNECTED_FAR', 'Trailer disconnected! Recover the trailer to complete your job and get payed!')
+    AddTextEntry('TJ_TRAILER_DISCONNECTED_FAR', 'Trailer disconnected! Recover the trailer to complete your job and get paid!')
     AddTextEntry('TJ_TRAILER_DISCONNECTED_CLOSE', 'Trailer disconnected! Disconnect the trailer when the marker turns green.')
 
-    AddTextEntry('TJ_FAILED_TRUCK_DESTROYED', '')
-
+    AddTextEntry('TJ_ROUTE_NOT_COMPLETE', 'You can\'t continue your shift because your previous route has not been completed')
     AddTextEntry('TJ_WAITING_FOR_ROUTE_ASSIGNMENT', 'Waiting for route to be assigned...')
     AddTextEntry('TJ_WAITING_FOR_ROUTE_INIT', 'Waiting for route initialization...')
     AddTextEntry('TJ_INCORRECT_TRUCK', 'You are not in the correct delivery vehicle. Please enter the one marked on your map.')
